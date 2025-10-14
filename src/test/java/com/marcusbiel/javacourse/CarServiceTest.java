0f27a3b15a73c8989a38ef135dc2e94dacf904ab
Samuel Nadim <wnadim2992@gmail.com>
2025-10-13 23:12:29 -0400

    update

--- src/test/java/com/marcusbiel/javacourse/CarServiceTest.java
+++ src/test/java/com/marcusbiel/javacourse/CarServiceTest.java
@@ -3,4 +3,12 @@ package com.marcusbiel.javacourse;
 import org.junit.Test;
 
 public class CarServiceTest {
+
+    @Test
+    public void shouldDemonstrateLogging() {
+        CarService carService = new CarService();
+        carService.process("BMW");
+
+    }
+
 }
