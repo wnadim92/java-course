b3598cac1c56eb7e9b9ac3da65b1ac04d8e7012a
Samuel Nadim <wnadim2992@gmail.com>
2025-10-23 21:03:03 -0400

    add main method

--- src/main/java/com/marcusbiel/javacourse/CarSelector.java
+++ src/main/java/com/marcusbiel/javacourse/CarSelector.java
@@ -3,7 +3,8 @@ package com.marcusbiel.javacourse;
 public class CarSelector {
 
     public static void main(String[] args) {
-
+        CarService carService = new CarService();
+        carService.process("BMW");
     }
 
 }
--- src/main/java/com/marcusbiel/javacourse/CarService.java
+++ src/main/java/com/marcusbiel/javacourse/CarService.java
@@ -8,7 +8,7 @@ public class CarService {
     private final Logger log = LoggerFactory.getLogger(CarService.class);
 
     public void process(String input) {
-        log.error("processing car {}", input);
+        log.info("processing car {}", input);
     }
 
 }
