66d1454d1fb935e88250ba44a09e3c7951959b47
Samuel Nadim <wnadim2992@gmail.com>
2025-10-23 21:14:02 -0400

    add dynamic car processing

--- src/main/java/com/marcusbiel/javacourse/CarSelector.java
+++ src/main/java/com/marcusbiel/javacourse/CarSelector.java
@@ -5,7 +5,7 @@ public class CarSelector {
     public static void main(String[] args) {
         CarService carService = new CarService();
         for (String arg : args) {
-            carService.process("BMW");
+            carService.process(arg);
         }
     }
 
