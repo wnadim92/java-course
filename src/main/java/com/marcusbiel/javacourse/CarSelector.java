b11b9a25a60f5abf33b29c1146d4db69c6176747
Samuel Nadim <wnadim2992@gmail.com>
2025-10-23 21:17:40 -0400

    update main from array paramater to accept n strings directly

--- src/main/java/com/marcusbiel/javacourse/CarSelector.java
+++ src/main/java/com/marcusbiel/javacourse/CarSelector.java
@@ -2,7 +2,7 @@ package com.marcusbiel.javacourse;
 
 public class CarSelector {
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         CarService carService = new CarService();
         for (String arg : args) {
             carService.process(arg);
