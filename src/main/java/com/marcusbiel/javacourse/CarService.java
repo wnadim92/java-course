75503714ef1b8c64a3f6e6801c4d376dd2ea822e
Samuel Nadim <wnadim2992@gmail.com>
2025-10-23 20:43:06 -0400

    test log levels

--- src/main/java/com/marcusbiel/javacourse/CarService.java
+++ src/main/java/com/marcusbiel/javacourse/CarService.java
@@ -8,7 +8,7 @@ public class CarService {
     private final Logger log = LoggerFactory.getLogger(CarService.class);
 
     public void process(String input) {
-        log.warn("processing car {}", input);
+        log.error("processing car {}", input);
     }
 
 }
