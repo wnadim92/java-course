d8353d7088c881b2788a5d3ebafec055ba19c1b3
Samuel Nadim <wnadim2992@gmail.com>
2025-10-25 12:23:34 -0400

    add test on main

--- src/test/java/com/marcusbiel/javacourse/CarSelectorTest.java
+++ src/test/java/com/marcusbiel/javacourse/CarSelectorTest.java
@@ -1,4 +1,12 @@
 package com.marcusbiel.javacourse;
 
+import org.junit.Test;
+
 public class CarSelectorTest {
+
+    @Test
+    public void shouldCallMain() {
+        String[] arguments = {"BMW", "Porsche"};
+        CarSelector.main(arguments);
+    }
 }
